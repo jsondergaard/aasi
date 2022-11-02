7e181581b35d4a7071140cfc5b1be336425a2463
Jacob Søndergaard <jacob@cavas.dk>
2022-11-02 13:31:11 +0100

    Update create.blade.php

--- resources/views/admin/sponsors/offers/create.blade.php
+++ resources/views/admin/sponsors/offers/create.blade.php
@@ -9,7 +9,7 @@
             @csrf
 
             <div class="row mb-3">
-                <label for="name" class="col-md-4 col-form-label text-md-end">Sponsor Navn</label>
+                <label for="name" class="col-md-4 col-form-label text-md-end">Navn</label>
 
                 <div class="col-md-6">
                     <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
@@ -17,8 +17,7 @@
                 </div>
             </div>
             <div class="row mb-3">
-                <label for="name" class="col-md-4 col-form-label text-md-end">Kupon Beskrivelse</label>
-
+                <label for="description" class="col-md-4 @error('description') is-invalid @enderror">Beskrivelse</label>
 
                 <div class="col-md-6">
                 <textarea name="description" class="form-control" rows="10">{{ old('description') }}</textarea>  
@@ -28,7 +27,7 @@
             <div class="row mb-0">
                 <div class="col-md-8 offset-md-4">
                     <button type="submit" class="btn btn-primary">
-                        Gem Kupon
+                        Gem
                     </button>
                 </div>
             </div>
