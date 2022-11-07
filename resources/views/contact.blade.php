2cc93cd8ea68668cba5f7988ff05d1ed244c778d
Jacob Søndergaard <jacob@cavas.dk>
2022-11-07 14:49:32 +0100

    kontakt os side

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,71 +2,55 @@
 
 
 @section('main')
-<div class="container mt-4">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="contact-form">
-                <h1>Contact Us</h1>
-                <p class="hint-text">We'd love to hear from you, please drop us a line if you've any query or question.
-                </p>
-                <form action="/examples/actions/confirmation.php" method="post">
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-12">
+                <h1>Kontakt os</h1>
+
+                <form action="#" method="post">
                     <div class="row">
-                        <div class="col-sm-4">
-                            <div class="form-group">
-                                <label for="inputNavn">Navn</label>
-                                <input type="text" class="form-control" id="inputNavn" required>
-                            </div>
+                        <div class="col-6">
+                            <label for="name">Navn</label>
+                            <input type="text" class="form-control" id="name" name="name" required>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-4">
-                        <select class="form-select" id="inputSelect">
-                            <option selected>Vælg afdeling</option>
-                            <option value="Badminton">Badminton</option>
-                            <option value="Fodbold">Fodbold</option>
-                            <option value="Håndbold">Håndbold</option>
-                            <option value="Svømning">Svømning</option>
-                            <option value="Volley">Volley</option>
-                            <option value="Andet">Andet</option>
-                        </select>
-                    </div>
+
+                        <div class="col-6">
+                            <label for="department">Afdeling</label>
+                            <select class="form-select" id="department" name="department">
+                                <option selected>Vælg afdeling</option>
+                                <option value="Badminton">Badminton</option>
+                                <option value="Fodbold">Fodbold</option>
+                                <option value="Håndbold">Håndbold</option>
+                                <option value="Svømning">Svømning</option>
+                                <option value="Volley">Volley</option>
+                                <option value="Andet">Andet</option>
+                            </select>
+                        </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-sm-4">
+                    <div class="row mb-3">
+                        <div class="col-6">
                             <div class="form-group">
-                                <label for="inputEmail">Email</label>
-                                <input type="email" class="form-control" id="inputEmail" required>
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" id="email" name="name" required>
                             </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <div class="form-group">
-                                <label for="inputTelefon">Telefon</label>
-                                <input type="text" class="form-control" id="inputTelefon" required>
-                            </div>
+
+                        <div class="col-6">
+                            <label for="subject">Emne</label>
+                            <input type="text" class="form-control" id="subject" name="subject" required>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label for="inputEmne">Emne</label>
-                        <input type="text" class="form-control" id="inputEmne" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="inputBesked">Besked</label>
-                        <textarea class="form-control" id="inputBesked" rows="5" required></textarea>
+
+                    <div class="row mb-3">
+                        <div class="col-12">
+                            <label for="inputBesked">Besked</label>
+                            <textarea class="form-control" id="inputBesked" rows="5" required></textarea>
+                        </div>
                     </div>
                     <button type="submit" class="btn btn-primary"><i class="fa fa-paper-plane"></i> Send</button>
                 </form>
             </div>
         </div>
     </div>
-</div>
-</body>
-
-</html>
-
-
-</form>
-</div>
 @endsection
