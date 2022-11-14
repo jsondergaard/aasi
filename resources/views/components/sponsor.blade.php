5ae983185e7c03cdeed3ec8f5222b4c9f10600d3
camillabramsen <38404673+camillabramsen@users.noreply.github.com>
2022-11-14 11:54:22 +0100

    fix-sponsor sites and carousel

--- resources/views/components/sponsor.blade.php
+++ resources/views/components/sponsor.blade.php
@@ -2,7 +2,7 @@
     <div class="carousel"
         data-flickity='{ "freeScroll": true, "wrapAround": true, "prevNextButtons": false, "pageDots": false, "autoPlay": true}'>
         @foreach ($sponsors as $sponsor)
-            <div class="carousel-cell" style="background-image: url('https://via.placeholder.com/150')"></div>
+            <div class="carousel-cell sponsor-cell" style="background-image: url('{{ $sponsor->imagePath }}')"></div>
         @endforeach
     </div>
 </div>
