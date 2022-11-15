6551e01147974c93e03114c1586dd538a1820f5c
Jacob Søndergaard <jacob@cavas.dk>
2022-11-15 14:22:03 +0100

    Hamburger

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,36 +1,46 @@
 <nav class="py-2 bg-light border-bottom">
-    <div class="container d-flex flex-wrap">
-        <ul class="nav me-auto">
-            @foreach (\App\Models\Page::orderBy('order_id')->get() as $page)
-                @if ($page->children->count() > 0)
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="{{ $page->path }}" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            {{ $page->name }}
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu">
-                            @if ($page->is_page)
-                                <li>
-                                    <a class="dropdown-item" href="{{ $page->path }}">{{ $page->name }}</a>
-                                </li>
-                            @endif
-                            @foreach (\App\Models\Page::where('parent_id', $page->id)->orderBy('order_id')->get() as $child)
-                                <li>
-                                    <a class="dropdown-item" href="{{ $child->path }}">{{ $child->name }}</a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </li>
-                @endif
+    <div class="container d-flex flex-wrap justify-content-between">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+            </svg>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="nav me-auto">
+                @foreach (\App\Models\Page::orderBy('order_id')->get() as $page)
+                    @if ($page->children->count() > 0)
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="{{ $page->path }}" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                {{ $page->name }}
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu">
+                                @if ($page->is_page)
+                                    <li>
+                                        <a class="dropdown-item" href="{{ $page->path }}">{{ $page->name }}</a>
+                                    </li>
+                                @endif
+                                @foreach (\App\Models\Page::where('parent_id', $page->id)->orderBy('order_id')->get() as $child)
+                                    <li>
+                                        <a class="dropdown-item" href="{{ $child->path }}">{{ $child->name }}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endif
 
-                @if ($page->parent_id == 0 && $page->children->count() == 0)
-                    <li class="nav-item">
-                        <a href="{{ $page->path }}" class="nav-link px-2">{{ $page->name }}</a>
-                    </li>
-                @endif
-            @endforeach
-            <li class="nav-item"><a href="{{ route('contact') }}" class="nav-link px-2">Kontakt</a></li>
-        </ul>
+                    @if ($page->parent_id == 0 && $page->children->count() == 0)
+                        <li class="nav-item">
+                            <a href="{{ $page->path }}" class="nav-link px-2">{{ $page->name }}</a>
+                        </li>
+                    @endif
+                @endforeach
+                <li class="nav-item"><a href="{{ route('contact') }}" class="nav-link px-2">Kontakt</a></li>
+            </ul>
+        </div>
         <ul class="nav">
             @guest
                 <li class="nav-item"><a href="{{ route('login') }}" class="nav-link px-2">Log ind</a></li>
